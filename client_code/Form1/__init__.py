5b969cdda59af115b4d119922c02f5ff8cdb98d3
ana.scepanovic57@yahoo.com <ana.scepanovic57@yahoo.com>
2026-07-22 15:34:55 +0000

    Created replace_e0.0.0.1.py; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,3 +5,8 @@ from anvil import *
 class Form1(Form1Template):
   def __init__(self, **properties):
     super().__init__(**properties)
+
+  @handle("button_1", "click")
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.text_area_2.text=self.text_area_1.text
