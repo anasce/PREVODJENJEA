fa8b2a8784dc80f1fb029fdbfc55c8f39d510f83
ana.scepanovic57@yahoo.com <ana.scepanovic57@yahoo.com>
2026-07-22 18:30:00 +0000

    Created ServerModule1; Edited Layouts, Form1 and Layouts.BaseLayout

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -9,4 +9,9 @@ class Form1(Form1Template):
   @handle("button_1", "click")
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.text_area_2.text=self.text_area_1.text
+    #self.text_area_2.text=self.text_area_1.text
+    # 2. Call the server function and store the returned value
+    result = anvil.server.call('process_data', text=self.text_area_1.text)
+
+    # 3. Use the result in your UI
+    self.text_area_2.text = result
