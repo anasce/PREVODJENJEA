0a1a7bd08bd6e5fbd8003ccb11420114a1f7fed0
anasce <97723156+anasce@users.noreply.github.com>
2026-08-13 14:37:35 +0200

    Fix comment formatting in __init__.py

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -25,7 +25,7 @@ class Form1(Form1Template):
     self.button_1.enabled = False
     #self.button_1.text = "Prevođenje..."
 
-    # Pošto je server pokrenut unaprijed dok je korisnik kucao, poziv je trenutan
+    #  Pošto je server pokrenut unaprijed dok je korisnik kucao, poziv je trenutan
     result = anvil.server.call('ijekavizuj_tekst', ulazni_tekst=self.text_area_1.text)
 
     # Prikazujemo rezultat i vraćamo dugme u funkciju
