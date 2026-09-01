081bfbe74633c613774d679e5ae1becd0cd05156
anasce <97723156+anasce@users.noreply.github.com>
2026-09-01 10:43:38 +0200

    Refactor import statement for Prevodilac

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,8 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
-from .. import Prevodilac
-
+import Prevodilac
 
 class Form1(Form1Template):
   def __init__(self, **properties):
